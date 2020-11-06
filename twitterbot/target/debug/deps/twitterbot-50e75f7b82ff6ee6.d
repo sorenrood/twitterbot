7ad7ba6d8e58cf208a9a33f6516d644b0a00c58c
Soren Rood <sorenrood@gmail.com>
2020-11-06 00:05:00 -0800

    added rust project

---
+++ twitterbot/target/debug/deps/twitterbot-50e75f7b82ff6ee6.d
@@ -0,0 +1,5 @@
+c:\Users\soren\dev\personal\twitterbot\twitterbot\target\debug\deps\twitterbot-50e75f7b82ff6ee6.rmeta: src\main.rs
+
+c:\Users\soren\dev\personal\twitterbot\twitterbot\target\debug\deps\twitterbot-50e75f7b82ff6ee6.d: src\main.rs
+
+src\main.rs:
---
+++ twitterbot/target/debug/deps/twitterbot-d709cafd8365ca48.d
@@ -0,0 +1,5 @@
+c:\Users\soren\dev\personal\twitterbot\twitterbot\target\debug\deps\twitterbot-d709cafd8365ca48.rmeta: src\main.rs
+
+c:\Users\soren\dev\personal\twitterbot\twitterbot\target\debug\deps\twitterbot-d709cafd8365ca48.d: src\main.rs
+
+src\main.rs:
