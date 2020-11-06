5642446d6cd2053f7c2d4b7e5970245a9f63ac8f
Soren Rood <sorenrood@gmail.com>
2020-11-05 22:24:34 -0800

    initial commit

---
+++ tweetbot.py
@@ -0,0 +1,11 @@
+# Twitter api
+
+def main():
+    api_key = 'WbJD4ltA9kbIAq3gFzIA5v6t7'
+    api_secret = '9V6WOkQxIPKbQMqePQzWzkaJgN5c5kWaYM2NCbFCvyNBVg4PKw'
+    bearer_token = 'AAAAAAAAAAAAAAAAAAAAAH8LIQEAAAAAO4La9O%2FS1DC%2B9N6%2FGTf1GCPK600%3DV73qqkoTgZoNm8t07No9kJ8qirH7XGoWEKIzKQv7QyDcyTVpIT'
+    print(api_key)
+
+
+if __name__ == "__main__":
+    main()
